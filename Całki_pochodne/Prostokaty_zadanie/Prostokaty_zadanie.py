c43b0642a8e4325becb533c8c24d0c10d1f768f6
Fedoseev Maksim <maksim.fedoseev2022@technikum19.edu.pl>
2024-01-21 23:12:08 +0100

    update Prostokaty_zadanie

--- Całki_pochodne/Prostokaty_zadanie/Prostokaty_zadanie.py
+++ Całki_pochodne/Prostokaty_zadanie/Prostokaty_zadanie.py
@@ -1,5 +1,26 @@
 def f(x):
     return 14 - 3*x
 
+# Przedzial a--b
+# n - ilosc sekcji
 def pros(a, b, n):
+    dx = (b - a) / n
+    suma = 0
+    for i in range(n):
+        suma += f(a + i * dx + dx / 2) * dx
+    return suma
 
+print(pros(1, 4, 4))
+
+
+def g(x):
+    return -3*x - 2
+
+def calka(p, k, n):
+    dx = (k - p) / n
+    suma = 0
+    for i in range(n):
+        suma += g(p + dx * i + dx / 2) * dx
+    return suma
+
+print(calka(-4, -1, 4))
