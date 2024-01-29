eae190ac3cdea889dbbd8bfa6350aae74f8f25a5
Maksim Fedoseev <114389643+fedoseevm@users.noreply.github.com>
2024-01-29 14:09:09 +0100

    Add files via upload

--- Całki_pochodne/Prostokaty_1/Prostokaty_1.py
+++ Całki_pochodne/Prostokaty_1/Prostokaty_1.py
@@ -27,6 +27,21 @@ print(prostokaty_srodek(2,5,4))
 print(prostokaty_poczatek(2,5,4))
 print(prostokaty_koniec(2,5,4))
 print("\n");
-print(prostokaty_srodek(2,5,4))
-print(prostokaty_poczatek(2,5,40))
-print(prostokaty_koniec(2,5,400))
+
+def trapez(a, b, n):
+    dx = (b - a) / n
+    suma = 0
+    for i in range(n):
+        suma += (f(a + i * dx) + f(a + i * dx + dx)) / 2 * dx
+    return suma
+
+print(trapez(2, 5, 4))
+
+def trapez_2(a, b, n):
+    dx = (b - a) / n
+    suma = f(a) + f(b)
+    for i in range(1, n):
+        suma += 2 * f(a + i * dx)
+    return suma / 2 * dx
+
+print(trapez_2(2, 5, 4))
