1ac142fb08fb8e22c2682b7d5c1156ef03ac72b2
fedoseevm <maksim.fedoseev122@gmail.com>
2024-02-05 13:39:25 +0100

    add Algorytm_Newton-raphson

---
+++ Algorytm_Newton-Raphson/Algorytm_Newton_Raphson.py
@@ -0,0 +1,26 @@
+# p - liczba, z ktorej szukamy pierwiastek
+# n - ilosc krokow
+def newton_raphson1(p, epsilon):
+    a = p
+    b = 1
+    #for i in range(n):
+    #    a = (a + b) / 2
+    #    b = p / a
+    while a - b > epsilon:
+        a = (a + b) / 2
+        b = p / a
+    return a
+
+print(newton_raphson1(4900, 0.000001))
+
+
+
+def newton_raphson2(p, n):
+    a = p
+    b = 1
+    for i in range(n):
+        a = (a + b) / 2
+        b = p / a
+    return a
+
+print(newton_raphson2(4900, 7))
