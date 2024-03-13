64e696bcf00e4d09584b467a1a07fec35f06fcdf
fedoseevm <maksim.fedoseev122@gmail.com>
2024-03-13 12:16:04 +0100

    Update Metoda_bisekcji

--- Całki_pochodne/Metoda_bisekcji/Metoda_bisekcji.py
+++ Całki_pochodne/Metoda_bisekcji/Metoda_bisekcji.py
@@ -1,8 +1,10 @@
 # Metoda bisekcji
-def f(x):
+def f(x):   # Definiowanie funkcji 
    return x ** 2 - 8 * x + 12
 
-def bisekcja(a, b, epsilon):
+# Metoda bisekcji - poszukiwanie miejsca zerowego funkcji, czyli takiego x, dla ktorego f(x) = 0
+# 2 warianty
+def bisekcja(a, b, epsilon):    # epsilon - o ile precyzyjny ma byc wynik
     while abs(b - a) > epsilon:
         c = (a + b) / 2
         if f(c) == 0:
@@ -12,8 +14,9 @@ def bisekcja(a, b, epsilon):
         else:
             a = c
     return (a + b) / 2
+print(bisekcja(-5, 50, 0.1))
 
-def bisekcja_2(a, b, n):
+def bisekcja_2(a, b, n):    # n - ilosc krokow
     for i in range(n):
         c = (a + b) / 2
         if f(c) == 0:
